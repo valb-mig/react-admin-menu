8146d727023c0fadab6987dc3ffbe2f27e7c352f
Mig <mig@pop-os.localdomain>
2023-02-12 18:01:18 -0300

    [Fixing Dashboard]

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -59,35 +59,44 @@ const Dashboard = () => {
     }
 
     return (
-        <>
-            <div className='dashboard'>
+        <div className='dashboard'>
+            <div className='buttons'>
+                <div className='date-button'>
+                </div>
+                <div className='filter-button'>
+                </div>
+            </div>
+            <div className='dashboard-context'>
+                <div className='context'>
 
-                <div className='dashboard-bundle'>
-                <div className='dashboard-out'>
-                    <Bar className='dashboard-content' options={chartOptions} data={chartData} height={`100px`} />
-                    <div className='bar'>
-                        <div className='dashboard-months'>
-                            <div className='months'>
-                            <p>Jan</p>
-                            <div className='actual-month'>Fev</div>
-                            <p>Mar</p>
-                            <p>Abr</p>
-                            <p>Mai</p>
-                            <p>Jun</p>
-                            <p>Jul</p>
-                            <p>Ago</p>
-                            <p>Set</p>
-                            <p>Out</p>
-                            <p>Nov</p>
-                            <p>Dez</p>
-                            </div>
+                </div>
+            </div>
+
+            <div className='dashboard-out'>
+               
+                <Bar className='dashboard-content' options={chartOptions} data={chartData} height={`100px`} />
+  
+                <div className='bar'>
+                    <div className='dashboard-months'>
+                        <div className='months'>
+                        <p>Jan</p>
+                        <div className='actual-month'>Fev</div>
+                        <p>Mar</p>
+                        <p>Abr</p>
+                        <p>Mai</p>
+                        <p>Jun</p>
+                        <p>Jul</p>
+                        <p>Ago</p>
+                        <p>Set</p>
+                        <p>Out</p>
+                        <p>Nov</p>
+                        <p>Dez</p>
                         </div>
                     </div>
                 </div>
-                </div>
 
             </div>
-        </>
+        </div>
     )
 }
 
