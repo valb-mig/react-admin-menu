ea7e46ddd76154d2cdec2c4ca2130c9a92c1e4b0
Valb <mig@pop-os.localdomain>
2023-02-27 20:41:31 -0300

    [Done by now]

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -4,6 +4,13 @@ import './css/greeting.css'
 function Greeting() {
   return (
     <div className='greeting'>
+      <div className='text'>
+          <i className='fa fa-quote-left icon-quote'></i>
+            Welcome to the Admin's homepage
+          <i className='fa fa-quote-left icon-quote'></i>
+        </div>
+      <i className='fa fa-cloud icon-left'></i>
+      <i className='fa fa-cloud icon'></i>
     </div>
   )
 }
